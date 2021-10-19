78e73eabbe6a21aedf5ef593c0d58b4918ed6d21
Brad Slater <bslater@financialforce.com>
2021-10-19 14:46:28 +0100

    slight changes

--- TCRMCore/src/config/parse.ts
+++ TCRMCore/src/config/parse.ts
@@ -3,16 +3,32 @@
  */
 
 import { Config, ConfigSchema } from "./config";
-import { Validator } from "jsonschema";
+import { ValidationError, Validator } from "jsonschema";
+
+function jsonParseError(): never {
+    throw new Error("The config field must be a valid json object");
+}
+
+function humanReadableValidationError(err: ValidationError): never {
+    const message: string = `Validation failed for config, path: "${err.path.join(".")}", property: ${err.property}, message: ${err.message}`;
+    throw new Error(message);
+}
 
 export function parseConfig(config: string): Config {
-    const parsedJson: unknown = JSON.parse(config),
-        validator = new Validator();
-
-    validator.validate(parsedJson, ConfigSchema, {
-        throwError: true,
-        throwAll: true
-    });
-
+    let parsedJson: unknown;
+    try {
+        parsedJson = JSON.parse(config);
+    } catch (err) {
+        jsonParseError();
+    }
+    try {
+        const validator = new Validator();
+        validator.validate(parsedJson, ConfigSchema, {
+            throwError: true,
+            throwAll: true
+        });
+    } catch (err) {
+        humanReadableValidationError(err);
+    }
     return parsedJson as Config;
 }
