ae898e00f11f43521e2bd65c11fbc2828032e205
Brad Slater <bslater@financialforce.com>
2021-10-19 09:51:40 +0100

    poc and libs

--- force-app/main/default/lwc/resonate/resonate.js
+++ force-app/main/default/lwc/resonate/resonate.js
@@ -2,11 +2,28 @@
  * Copyright (c) 2020 FinancialForce.com, inc. All rights reserved.
  */
 
-import { LightningElement, api } from "lwc";
+import { LightningElement, api, track } from "lwc";
 
 class Resonate extends LightningElement {
 
     @api title;
+    @api getState;
+    @api setState;
+
+    @track state;
+    @track store;
+
+    connectedCallback() {
+        if (!window.ffdcStore) {
+            window.ffdcStore = "Secret" + Math.random();
+        }
+        this.state = JSON.stringify(this.getState());
+        this.store = window.ffdcStore;
+    }
+
+    handleRefresh() {
+        this.state = JSON.stringify(this.getState());
+    }
 
 }
 
