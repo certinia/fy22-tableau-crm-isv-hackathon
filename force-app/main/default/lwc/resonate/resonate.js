a8acf1e3808942c07025a10ed3bb4bbe870da981
Brad Slater <bslater@financialforce.com>
2021-10-19 08:59:05 +0100

    new component

---
+++ force-app/main/default/lwc/resonate/resonate.js
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020 FinancialForce.com, inc. All rights reserved.
+ */
+
+import { LightningElement, api } from "lwc";
+
+class Resonate extends LightningElement {
+
+    @api title;
+
+}
+
+export default Resonate;
