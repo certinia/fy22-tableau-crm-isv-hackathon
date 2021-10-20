a71188329b58beb37748fe3711a11427da2ffb59
Brad Slater <bslater@financialforce.com>
2021-10-20 11:44:38 +0100

    renamed stuff

--- TCRMCore/src/index.ts
+++ TCRMCore/src/index.ts
@@ -2,9 +2,9 @@
  * Copyright (c) 2021 FinancialForce.com, inc. All rights reserved.
  */
 
-import Engine from "./engine/core";
+import Model from "./engine/model";
 import { Params } from "./types";
 
-export function createEngine(input: Params): Engine {
-    return new Engine(input);
+export function createModel(input: Params): Model {
+    return new Model(input);
 }
